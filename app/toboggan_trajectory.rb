1147cf1cbfda2a0d7fbff061517455174dbc7e8d
mhuerster <mary.huerster@gmail.com>
2020-12-04 01:44:10 -0500

    Solve Day 3, Part 2

--- app/toboggan_trajectory.rb
+++ app/toboggan_trajectory.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class TobogganTrajectory
   attr_reader :map_filename, :x_increment, :y_increment
   TREE = '#'.freeze
@@ -10,22 +9,46 @@ class TobogganTrajectory
   end
 
   def count_trees
-    map.each_with_index.reduce(0) do |tree_count, coords|
-      row, x = coords
+    input_map.each_with_index.reduce(0) do |tree_count, coords|
+      row, idx = coords
 
       # due to something you read about once involving arboreal genetics and biome stability, the
       # same pattern repeats to the right many times
-      # modulo is for the pattern repeat
-      col = (x*x_increment) % row.length
+      # modulo row length is for the pattern repeat
+      x_displacement = ((idx*x_increment)/y_increment) % row.length
 
-      return tree_count unless x % y_increment == 0
-      row[col] == TREE ? tree_count + 1 : tree_count
+      if idx % y_increment == 0 && row[x_displacement] == TREE
+        tree_count + 1
+      else
+        tree_count
+      end
     end
   end
 
   private
 
-  def map
-    @map ||= File.readlines(map_filename, chomp: true)
+  def input_map
+    @input_map ||= File.readlines(map_filename, chomp: true)
   end
 end
+
+## Part 2 script ##
+#
+# Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal
+# stop, after all.
+# Determine the number of trees you would encounter if, for each of the following slopes, you
+# start at the top-left corner and traverse the map all the way to the bottom:
+
+#CANDIDATES = [
+  #[1, 1], #Right 1, down 1.
+  #[3, 1], #Right 3, down 1. (This is the slope you already checked.)
+  #[5, 1], #Right 5, down 1.
+  #[7, 1], #Right 7, down 1.
+  #[1, 2], #Right 1, down 2.
+#].freeze
+
+#CANDIDATES.reduce(0) do |product, candidate|
+  #x, y = candidate
+  #instance = TobogganTrajectory.new(x, y, 'spec/fixtures/toboggan_trajectory/actual.txt')
+  #product * instance.count_trees
+#end
--- spec/toboggan_trajectory_spec.rb
+++ spec/toboggan_trajectory_spec.rb
@@ -1,14 +1,27 @@
 require_relative '../app/toboggan_trajectory'
+require 'pry'
 
 RSpec.describe TobogganTrajectory do
   let(:map_filename) { 'spec/fixtures/toboggan_trajectory/test.txt' }
-  let(:x) { 3 }
-  let(:y) { 1 }
   let(:instance) { described_class.new(x, y, map_filename) }
 
   describe 'count_trees' do
     subject { instance.count_trees }
 
-    it { is_expected.to eq(7) }
+    # Given test cases
+    [
+      [1, 1, 2],
+      [3, 1, 7],
+      [5, 1, 3],
+      [7, 1, 4],
+      [1, 2, 2],
+    ].each do |x, y, expected_count|
+      context "given right #{x}, down #{y}" do
+        let(:x) { x }
+        let(:y) { y }
+
+        it { is_expected.to eq(expected_count) }
+      end
+    end
   end
 end
