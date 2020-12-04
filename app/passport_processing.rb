f33c98ca80bf8b17cbaa36b2529fc3a5a088d4e6
mhuerster <mary.huerster@gmail.com>
2020-12-04 03:34:51 -0500

    Solve Day 4 Part 2

--- app/passport_processing.rb
+++ app/passport_processing.rb
@@ -1,17 +1,18 @@
 class PassportProcessing
   attr_reader :batch_filename
+  class Field < Struct.new(:code, :validator); end
 
   PASSPORT_SEPARATOR = "\n\n".freeze
   FIELD_SEPARATOR    = ":".freeze
 
-  BIRTH_YEAR      = 'byr'.freeze
-  ISSUE_YEAR      = 'iyr'.freeze
-  EXPIRATION_YEAR = 'eyr'.freeze
-  HEIGHT          = 'hgt'.freeze
-  HAIR_COLOR      = 'hcl'.freeze
-  EYE_COLOR       = 'ecl'.freeze
-  PASSPORT_ID     = 'pid'.freeze
-  COUNTRY_ID      = 'cid'.freeze
+  BIRTH_YEAR      = Field.new('byr', :valid_birth_year?).freeze
+  ISSUE_YEAR      = Field.new('iyr', :valid_issue_year?).freeze
+  EXPIRATION_YEAR = Field.new('eyr', :valid_expiration_year?).freeze
+  HEIGHT          = Field.new('hgt', :valid_height?).freeze
+  HAIR_COLOR      = Field.new('hcl', :valid_hair_color?).freeze
+  EYE_COLOR       = Field.new('ecl', :valid_eye_color?).freeze
+  PASSPORT_ID     = Field.new('pid', :valid_passport_id?).freeze
+  COUNTRY_ID      = Field.new('cid', :valid_country_id?).freeze
 
   ALL_PASSPORT_FIELDS = [
     BIRTH_YEAR,
@@ -35,10 +36,60 @@ class PassportProcessing
   end
 
   def valid?(passport_string)
-    REQUIRED_FIELDS.all? { |k| passport_data_hash(passport_string).key?(k) }
+    REQUIRED_FIELDS.all? do |field|
+      passport_value = passport_data_hash(passport_string)[field.code]
+      !passport_value.nil? && send(field.validator, passport_value)
+    end
+  end
+
+  # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+  def valid_birth_year?(str)
+    str.length == 4 && (1920..2002).cover?(str.to_i)
+  end
+
+  # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+  def valid_issue_year?(str)
+    str.length == 4 && (2010..2020).cover?(str.to_i)
+  end
+
+  # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+  def valid_expiration_year?(str)
+    str.length == 4 && (2020..2030).cover?(str.to_i)
+  end
+
+  # hgt (Height) - a number followed by either cm or in:
+  # If cm, the number must be at least 150 and at most 193.
+  # If in, the number must be at least 59 and at most 76.
+  def valid_height?(str)
+    value, unit = str.scan(/(\d+)(cm|in)/).flatten
+    if unit == 'cm'
+      (150..193).cover?(value.to_i)
+    elsif unit == 'in'
+      (59..76).cover?(value.to_i)
+    else
+      false
+    end
+  end
+
+  # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+  def valid_hair_color?(str)
+    /#([0-9]|[a-f]){6}/.match?(str)
+  end
+
+  # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+  def valid_eye_color?(str)
+    %w(amb blu brn gry grn hzl oth).include?(str)
+  end
+
+  # pid (Passport ID) - a nine-digit number, including leading zeroes.
+  def valid_passport_id?(str)
+    /\d{9}/.match?(str) && str.length == 9
   end
 
   private
+
+  # cid (Country ID) - ignored, missing or not.
+  def valid_country_id?(str); true; end
 
   # Each passport is represented as a sequence of key:value pairs separated by spaces or newlines.
   def passport_data_hash(passport_string)
--- spec/passport_processing_spec.rb
+++ spec/passport_processing_spec.rb
@@ -38,6 +38,37 @@ RSpec.describe PassportProcessing do
 
       it { is_expected.to eq(false) }
     end
+
+    # additional given examples
+    context 'known invalid examples' do
+      [
+        "eyr:1972 cid:100\n hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+        "iyr:2019\nhcl:#602927 eyr:1967 hgt:170cm\necl:grn pid:012533040 byr:1946",
+        "hcl:dab227 iyr:2012\necl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+        "hgt:59cm ecl:zzz\neyr:2038 hcl:74454a iyr:2023\npid:3556412378 byr:2007",
+      ].each do |test_string|
+        let(:passport_string) { test_string }
+
+        context "given #{test_string}" do
+          it { is_expected.to eq(false) }
+        end
+      end
+    end
+
+    context 'known valid examples' do
+      [
+        "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\nhcl:#623a2f",
+        "eyr:2029 ecl:blu cid:129 byr:1989\niyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+        "hcl:#888785\nhgt:164cm byr:2001 iyr:2015 cid:88\npid:545766238 ecl:hzl\neyr:2022",
+        "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+      ].each do |test_string|
+        let(:passport_string) { test_string }
+
+        context "given #{test_string}" do
+          it { is_expected.to eq(true) }
+        end
+      end
+    end
   end
 
   describe '#count_valid_passport_strings' do
@@ -45,4 +76,91 @@ RSpec.describe PassportProcessing do
       expect(instance.count_valid_passports).to eq (2)
     end
   end
+
+  describe '#valid_birth_year?' do
+    subject(:byr) { instance.valid_birth_year?(str) }
+
+    #byr valid:   2002
+    context 'when valid' do
+      let(:str) { '2002' }
+      it { is_expected.to eq(true) }
+    end
+
+    #byr invalid: 2003
+    context 'when invalid' do
+      let(:str) { '2003' }
+      it { is_expected.to eq(false) }
+    end
+  end
+
+  describe '#valid_height?' do
+    #hgt valid:   60in
+    #hgt valid:   190cm
+    context 'when valid' do
+      it 'is true' do
+        expect(instance.valid_height?('60in')).to eq(true)
+        expect(instance.valid_height?('190cm')).to eq(true)
+      end
+    end
+
+    #hgt invalid: 190in
+    #hgt invalid: 190
+    context 'when invalid' do
+      it 'is false' do
+        expect(instance.valid_height?('190in')).to eq(false)
+        expect(instance.valid_height?('190')).to eq(false)
+      end
+    end
+  end
+
+  describe '#valid_hair_color?' do
+    #hcl valid:   #123abc
+    context 'when valid' do
+      it 'is true' do
+        expect(instance.valid_hair_color?('#123abc')).to eq(true)
+      end
+    end
+
+    #hcl invalid: #123abz
+    #hcl invalid: 123abc
+    context 'when invalid' do
+      it 'is false' do
+        expect(instance.valid_hair_color?('#123abz')).to eq(false)
+        expect(instance.valid_hair_color?('123abc')).to eq(false)
+      end
+    end
+  end
+
+  describe '#valid_eye_color?' do
+    subject(:ecl) { instance.valid_eye_color?(str) }
+
+    #ecl valid:   brn
+    context 'when valid' do
+      let(:str) { 'brn' }
+      it { is_expected.to eq(true) }
+    end
+
+    #ecl invalid: wat
+    context 'when invalid' do
+      let(:str) { 'wat' }
+      it { is_expected.to eq(false) }
+    end
+  end
+
+  describe '#valid_passport_id?' do
+    subject(:pid) { instance.valid_passport_id?(str) }
+
+    #pid valid:   000000001
+    context 'when valid' do
+      let(:str) { '000000001' }
+      it { is_expected.to eq(true) }
+    end
+
+    #pid invalid: 0123456789
+    context 'when invalid' do
+      let(:str) { '0123456789' }
+      it { is_expected.to eq(false) }
+    end
+  end
+
 end
