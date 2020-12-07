51d6afa401ccdfa0d47c3f6ee57b6347c3572e7a
mhuerster <mary.huerster@gmail.com>
2020-12-07 11:06:56 -0500

    Solve Day 7 Parts 1 & 2

---
+++ app/handy_haversacks.rb
@@ -0,0 +1,42 @@
+class HandyHaversacks
+  CONTAINER = /\A(.*)bags\scontain.*\z/.freeze
+  CONTENTS  = /(\d+)\s(\w+\s\w+)\sbag/.freeze
+
+  attr_reader :input_filename
+  attr_accessor :rules
+
+  def initialize(input_filename)
+    @input_filename = input_filename
+    @rules = {}
+    parse_rules
+  end
+
+  def parse_rules
+    File.readlines(input_filename, chomp: true).each do |line|
+      container =  CONTAINER.match(line)[1].strip
+      contents = line.scan(CONTENTS)
+
+      rules[container] = {} unless rules.key?(container)
+      contents.each { |count, color| rules[container][color] = count.to_i }
+    end
+
+    rules
+  end
+
+  def containers(target_color)
+    direct = rules.select { |k, v| v.key?(target_color) }.keys
+    return direct if direct.empty?
+
+    (direct + direct.map { |color| containers(color) }.flatten).uniq
+  end
+
+  def contents_count(target_color)
+    contents = rules[target_color]
+    return 0 if contents.empty?
+
+    contents.reduce(0) do |sum, pair|
+      color, count = pair
+      sum + count + contents_count(color)*count
+    end
+  end
+end
---
+++ spec/handy_haversacs_spec.rb
@@ -0,0 +1,50 @@
+require_relative '../app/handy_haversacks'
+
+RSpec.describe HandyHaversacks do
+  let(:input_filename) { 'spec/fixtures/handy_haversacks/test.txt' }
+  let(:inst) { described_class.new(input_filename) }
+
+  describe '#parse_rules' do
+    subject(:rules) { inst.parse_rules }
+
+    context 'given test rules' do
+      let(:expected_rules) do
+        {
+          'light red' => { 'bright white' => 1, 'muted yellow' => 2 },
+          'dark orange' => { 'bright white' => 3, 'muted yellow' => 4 },
+          'bright white' => { 'shiny gold' => 1 },
+          'muted yellow' => { 'shiny gold' => 2, 'faded blue' => 9 },
+          'shiny gold' => { 'dark olive' =>1 , 'vibrant plum' => 2 },
+          'dark olive' => { 'faded blue' => 3, 'dotted black' => 4 },
+          'vibrant plum' => { 'faded blue' => 5, 'dotted black' => 6 },
+          'faded blue' => {},
+          'dotted black' => {},
+        }
+      end
+
+      it { is_expected.to eq(expected_rules) }
+    end
+  end
+
+  describe '#containers' do
+    subject(:containers) { inst.containers(target_color) }
+
+    context 'given shiny gold' do
+      let(:target_color) { 'shiny gold' }
+      let(:expected_containers) { ['bright white', 'dark orange', 'muted yellow', 'light red'] }
+
+      it { is_expected.to match_array(expected_containers) }
+    end
+  end
+
+  describe '#contents_count' do
+    subject(:contents) { inst.contents_count(target_color) }
+
+    context 'given shiny gold' do
+      let(:target_color) { 'shiny gold' }
+
+      it { is_expected.to eq(32) }
+    end
+  end
+end
+
