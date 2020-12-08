d1f01ba7d4b2fe676fe03a9bbb4c26cf297483b5
Molly Huerster <mollyhuerster@squareup.com>
2020-12-08 13:14:57 -0500

    Rubocop manual corrections

--- spec/passport_processing_spec.rb
+++ spec/passport_processing_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe PassportProcessing do
       it { is_expected.to eq(true) }
     end
 
-    context 'missing required height field' do
+    context 'when required height field is missing' do
       let(:passport_string) do
         "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929"
       end
@@ -23,7 +23,7 @@ RSpec.describe PassportProcessing do
       it { is_expected.to eq(false) }
     end
 
-    context 'missing optional country ID field' do
+    context 'when optional country ID field is missing' do
       let(:passport_string) do
         "hcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm"
       end
@@ -31,7 +31,7 @@ RSpec.describe PassportProcessing do
       it { is_expected.to eq(true) }
     end
 
-    context 'missing an optional and a required field' do
+    context 'when an optional and a required field are both missing' do
       let(:passport_string) do
         "hcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in"
       end
@@ -40,7 +40,7 @@ RSpec.describe PassportProcessing do
     end
 
     # additional given examples
-    context 'known invalid examples' do
+    context 'for known invalid examples' do
       [
         "eyr:1972 cid:100\n hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
         "iyr:2019\nhcl:#602927 eyr:1967 hgt:170cm\necl:grn pid:012533040 byr:1946",
@@ -55,7 +55,7 @@ RSpec.describe PassportProcessing do
       end
     end
 
-    context 'known valid examples' do
+    context 'for known valid examples' do
       [
         "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\nhcl:#623a2f",
         "eyr:2029 ecl:blu cid:129 byr:1989\niyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
@@ -96,39 +96,60 @@ RSpec.describe PassportProcessing do
   end
 
   describe '#valid_height?' do
+    subject(:valid_height) { instance.valid_height?(test_str) }
+
     # hgt valid:   60in
+    context 'given a value in the valid range in inches' do
+      let(:test_str) { '60in' }
+
+      it { is_expected.to eq(true) }
+    end
+
     # hgt valid:   190cm
-    context 'when valid' do
-      it 'is true' do
-        expect(instance.valid_height?('60in')).to eq(true)
-        expect(instance.valid_height?('190cm')).to eq(true)
-      end
+    context 'given a value in the valid range in cm' do
+      let(:test_str) { '190cm' }
+
+      it { is_expected.to eq(true) }
     end
 
     # hgt invalid: 190in
+    context 'given a value outside the valid range' do
+      let(:test_str) { '190in' }
+
+      it { is_expected.to eq(false) }
+    end
+
     # hgt invalid: 190
-    context 'when invalid' do
-      it 'is false' do
-        expect(instance.valid_height?('190in')).to eq(false)
-        expect(instance.valid_height?('190')).to eq(false)
-      end
+    context 'given a value without a unit' do
+      let(:test_str) { '190' }
+
+      it { is_expected.to eq(false) }
     end
   end
 
   describe '#valid_hair_color?' do
+    subject(:valid_hair_color) { instance.valid_hair_color?(test_str) }
+
     # hcl valid:   #123abc
     context 'when valid' do
-      it 'is true' do
-        expect(instance.valid_hair_color?('#123abc')).to eq(true)
-      end
+      let(:test_str) { '#123abc' }
+
+      it { is_expected.to eq(true) }
     end
 
-    # hcl invalid: #123abz
-    # hcl invalid: 123abc
     context 'when invalid' do
-      it 'is false' do
-        expect(instance.valid_hair_color?('#123abz')).to eq(false)
-        expect(instance.valid_hair_color?('123abc')).to eq(false)
+      # hcl invalid: #123abz
+      context 'given a character out of range' do
+        let(:test_str) { '#123abz' }
+
+        it { is_expected.to eq(false) }
+      end
+
+      # hcl invalid: 123abc
+      context 'when not prefixed with #' do
+        let(:test_str) { '123abc' }
+
+        it { is_expected.to eq(false) }
       end
     end
   end
